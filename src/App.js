629ff6a5ff2ecc7bc2b6a013c2268788d67160ef
Julian Nicholls <juliannicholls29@gmail.com>
2019-01-08 10:28:49 +0000

    Track mouse and Pass props to Canvas from App

--- src/App.js
+++ src/App.js
@@ -2,15 +2,33 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Canvas from './components/Canvas';
+import { getCanvasPosition } from './utils/formulas';
 
 class App extends Component {
+  componentDidMount() {
+    const self = this;
+    setInterval(() => {
+      self.props.moveObjects(self.canvasMousePosition);
+    }, 16);
+  }
+
+  trackMouse(event) {
+    this.canvasMousePosition = getCanvasPosition(event);
+  }
+
   render() {
-    return <Canvas />;
+    return (
+      <Canvas
+        angle={this.props.angle}
+        trackMouse={event => this.trackMouse(event)}
+      />
+    );
   }
 }
 
 App.propTypes = {
-  message: PropTypes.string.isRequired
+  angle: PropTypes.number.isRequired,
+  moveObjects: PropTypes.func.isRequired
 };
 
 export default App;
