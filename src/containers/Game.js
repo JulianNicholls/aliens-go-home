29ffff7cdbaea5a8914c21339cc0e7b26df10a46
Julian Nicholls <juliannicholls29@gmail.com>
2019-01-10 09:35:41 +0000

    Re-factor Game to removE MapDispatchToProps

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,23 +1,24 @@
 import { connect } from 'react-redux';
 
 import App from '../App';
-import { moveObjects, startGame } from '../actions';
+// import { moveObjects, startGame } from '../actions';
+import * as actions from '../actions';
 
 const mapStateToProps = state => ({
   angle: state.angle,
   gameState: state.gameState
 });
 
-const mapDispatchToProps = dispatch => ({
-  moveObjects: mousePosition => {
-    dispatch(moveObjects(mousePosition));
-  },
-  startGame: () => {
-    dispatch(startGame());
-  }
-});
+// const mapDispatchToProps = dispatch => ({
+//   moveObjects: mousePosition => {
+//     dispatch(moveObjects(mousePosition));
+//   },
+//   startGame: () => {
+//     dispatch(startGame());
+//   }
+// });
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  actions
 )(App);
